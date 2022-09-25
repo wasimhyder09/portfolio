738e632866507ce74a39a3bcd00410d21fe4382c
Wasim Hyder <wasim@creativefaze.com>
2022-09-25 21:44:40 +0500

    Started styling of menu items

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar">
         <div className="navbar__container">
-          <Link to={'/'} className="navbar__container_log">
+          <Link to={'/'} className="navbar__container__logo">
             <FaReact size={30} />
           </Link >
           <ul className={click ? "navbar__container__menu active" : "navbar__container__menu"}>
