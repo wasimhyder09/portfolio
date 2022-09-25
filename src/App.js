19081c1023789ab4ef54dd637f44d509a8d21443
Wasim Hyder <wasim@creativefaze.com>
2022-09-25 13:55:31 +0500

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './containers/home'
+import About from './containers/about'
+import Contact from './containers/contact'
+import Portfolio from './containers/portfolio'
+import Resume from './containers/resume'
+import Skills from './containers/skills'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
+      {/* particle js */}
+
+      {/* navbar component */}
+
+      {/* main page content */}
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
