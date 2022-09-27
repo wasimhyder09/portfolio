019fef534d11f465b6c5b8732601258b279c1159
Wasim Hyder <wasim@creativefaze.com>
2022-09-27 15:05:16 +0500

    Styled home page

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,3 +1,5 @@
+import {FaFacebook, FaLinkedin, FaInstagram, FaTwitter}  from 'react-icons/fa'
+import {Animate} from 'react-simple-animate'
 import './styles.scss'
 const Home = () => {
   return(
@@ -9,6 +11,30 @@ const Home = () => {
           Full Stack Developer.
         </h1>
       </div>
+      <Animate
+        play = {true}
+        duration= {1.5}
+        delay= {1}
+        start={{
+          transform: 'translateY(600px)'
+        }}
+        end={{
+          transform: 'translateX(0px)'
+        }}
+      >
+        <div className="contact-me">
+          <div className="contact-me__buttons-wrapper">
+            <button>Hire me</button>
+            <a href="">Download resume</a>
+          </div>
+          <div className="contact-me__socials-wrapper">
+            <FaLinkedin size={32} />
+            <FaFacebook size={32} />
+            <FaTwitter size={32} />
+            <FaInstagram size={32} />
+          </div>
+        </div>
+      </Animate>
     </section>
   )
 }
