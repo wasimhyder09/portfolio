aa7180ac329153a66b14358a0a4c95a62a5e0d37
Wasim Hyder <wasim@creativefaze.com>
2022-10-06 21:36:39 +0500

    Created skills section

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,8 +1,62 @@
+import {skillsData} from './utils'
+import PageHeaderContent from '../../components/pageHeaderContent'
+import {GiSkills} from 'react-icons/gi'
+import {Line} from 'rc-progress'
+import {Animate, AnimateKeyframes} from 'react-simple-animate'
+import './styles.scss'
+
 const Skills = () => {
   return(
-    <div>
-      Skills
-    </div>
+    <section id="skills" className="skills">
+      <PageHeaderContent
+        headerText = "My Skills"
+        icon = <GiSkills size={40} />
+      />
+      <div className="skills__content-wrapper">
+        {
+          skillsData.map((item, key) => (
+            <div key={key} className="skills__content-wrapper__inner-content">
+              <Animate
+                play
+                duration = {1}
+                delay = {0.4}
+                start = {{
+                  transform: 'translateX(-250px)'
+                }}
+                end = {{
+                  transform: 'translateX(0px)'
+                }}
+              >
+                <h3 className="skills__content-wrapper__inner-content__category-text">{item.label}</h3>
+                <div>
+                  {
+                    item.data.map((dataItem, index) => (
+                      <AnimateKeyframes
+                        play
+                        duration = {1}
+                        keyframes = {['opacity : 1', 'opacity : 0']}
+                        iterationCount = {1}
+                      >
+                        <div key={index} className="progressbar-wrapper">
+                          <p>{dataItem.skillName}</p>
+                          <Line
+                            percent={dataItem.percentage}
+                            strokeWidth = "3"
+                            strokeColor = "var(--selected-theme-main-color)"
+                            strokeLinecap = "square"
+                            trailWidth = "3"
+                          />
+                        </div>
+                      </AnimateKeyframes>
+                    ))
+                  }
+                </div>
+              </Animate>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,69 @@
+export const skillsData = [
+  {
+    label: 'FRONT END',
+    data: [
+      {
+        skillName: 'HTML',
+        percentage: '90'
+      },
+      {
+        skillName: 'CSS',
+        percentage: '80'
+      },
+      {
+        skillName: 'jQuery & JavaScript',
+        percentage: '85'
+      },
+      {
+        skillName: 'ReactJS',
+        percentage: '70'
+      },
+      {
+        skillName: 'BootStrap',
+        percentage: '75'
+      }
+    ]
+  },
+  {
+    label: 'BACK END',
+    data: [
+      {
+        skillName: 'PHP',
+        percentage: '90'
+      },
+      {
+        skillName: 'Drupal & WordPress',
+        percentage: '80'
+      },
+      {
+        skillName: 'Laravel',
+        percentage: '85'
+      },
+      {
+        skillName: 'RoR',
+        percentage: '70'
+      },
+      {
+        skillName: 'JAVA',
+        percentage: '65'
+      },
+      {
+        skillName: 'C#',
+        percentage: '50'
+      }
+    ]
+  },
+  {
+    label: 'DATABASES',
+    data: [
+      {
+        skillName: 'MySQL',
+        percentage: '90'
+      },
+      {
+        skillName: 'MongoDB',
+        percentage: '50'
+      }
+    ]
+  }
+]
