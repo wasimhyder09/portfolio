14cd7dad2eeed0a666f15e26b0c98c89b586005e
Wasim Hyder <wasim@creativefaze.com>
2022-10-08 21:56:00 +0500

    Created contact section

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,8 +1,35 @@
+import PageHeaderConent from '../../components/pageHeaderContent'
+import {RiContactsFill} from 'react-icons/ri'
+import './styles.scss'
+
 const Contact = () => {
   return(
-    <div>
-      Contact
-    </div>
+    <section id="contact" className="contact">
+      <PageHeaderConent
+        headerText = "Contact Me"
+        icon = {<RiContactsFill size={40} />}
+      />
+      <div className="contact__content">
+        <h3 className="contact__content__header-text">Let's Talk</h3>
+        <div className="contact__content__form">
+          <div className="contact__content__form__controlsWrapper">
+            <div className="nameWrapper">
+              <input name="name" className="inputName" type="text" />
+              <label htmlFor="name" className="nameLabel">Name</label>
+            </div>
+            <div className="emailWrapper">
+              <input name="email" className="inputEmail" type="email" />
+              <label htmlFor="email" className="emailLabel">Email</label>
+            </div>
+            <div className="descriptionWrapper">
+              <textarea name="description" className="inputDescription" type="text" rows={'5'} style={{resize: 'none'}} />
+              <label htmlFor="description" className="descriptionLabel">Description</label>
+            </div>
+          </div>
+          <button>Submit</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
